bd929a82d8f2dce14e17ce59566375c4a2025775
Kuma-0x0 <108416567+Kuma-0x0@users.noreply.github.com>
2024-04-27 01:03:48 +0900

    update setting files and functions (#2)

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -1,35 +1,25 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Mime;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
-namespace Company.Function
+namespace Company.Function;
+
+public class HttpTrigger(ILoggerFactory loggerFactory)
 {
-    public static class HttpTrigger
+    private readonly ILogger _logger = loggerFactory.CreateLogger<HttpTrigger>();
+
+    [Function("HttpTrigger")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req)
     {
-        [FunctionName("HttpTrigger")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", $"{MediaTypeNames.Text.Plain}; charset=utf-8");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        response.WriteString("Welcome to Azure Functions!");
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
+        return response;
     }
 }
